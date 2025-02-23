7d7e582bdf416b37dcc1419971a4551fb92b54bc
K1dou <hique1276@gmail.com>
2025-02-23 10:07:29 -0300

    add terraform.tfstate em um bucket s3

--- provider.tf
+++ provider.tf
@@ -5,6 +5,14 @@ terraform {
       version = "~> 5.0"
     }
   }
+
+  backend "s3" {
+    bucket = "devops"
+    key = "dev/terraform.tfstate"
+    region = "us-east-1"
+  }
+
+
 }
 
 # Configure the AWS Provider
